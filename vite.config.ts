a341cb5cf3be67894729ac36fcd1ca9ac9a224f1
wdj <dingjie.wang@supwisdom.com>
2023-07-16 15:05:55 +0800

    feat: vite and prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// 因为typeScript的原因引入path对象需要先安装types/node插件
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	// 给路径名称取别名
+	server: {
+		host: "localhost",
+		port: 8080,
+		proxy: {
+			"/api": "http://api-driver.marsview.cc",
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	plugins: [react()],
+});
