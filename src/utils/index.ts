9bd07448213cc08a3083aca18f2bf41760351d8f
wdj <dingjie.wang@supwisdom.com>
2023-08-18 09:55:19 +0800

    feat: add_api_list

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,3 +14,43 @@ export const formatNum = (num: number | string) => {
 	if (a.indexOf(".") > -1) return a.replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
 	return a.replace(/(\d)(?=(\d{3})+$)/g, "$1,");
 };
+
+// 格式化日期
+export const toLocalDate = (date?: Date, rule?: string) => {
+	let curDate = new Date();
+	if (date) curDate = date;
+	// String.replaceAll()方法太新了
+	if (rule === "yyyy-MM-dd")
+		return curDate.toLocaleDateString().split("/").join("-");
+	if (rule === "HH:mm:sss")
+		return curDate.toLocaleTimeString().split("/").join("-");
+	return curDate.toLocaleString().split("/").join("-");
+};
+
+// 格式化日期
+export const fomratDate = (date?: Date, rule?: string) => {
+	let curDate = new Date();
+	if (date) curDate = date;
+
+	let fmt = rule || "yyyy-MM-dd HH:mm:ss";
+	fmt = fmt.replace(/(y+)/, curDate.getFullYear().toString());
+	type OType = {
+		[key: string]: number;
+	};
+	const O: OType = {
+		"M+": curDate.getMonth() + 1,
+		"d+": curDate.getDate(),
+		"H+": curDate.getHours(),
+		"m+": curDate.getMinutes(),
+		"s+": curDate.getSeconds(),
+	};
+	for (const k in O) {
+		const val = O[k].toString();
+		fmt = fmt.replace(
+			new RegExp(`(${k})`),
+			O[k] > 9 ? O[k].toString() : "0" + O[k].toString()
+		);
+		// fmt = fmt.replace(new RegExp(`(${k})`), ('00' + val).substring(val.length))
+	}
+	return fmt;
+};
